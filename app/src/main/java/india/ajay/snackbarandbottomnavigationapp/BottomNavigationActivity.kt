1658471bfe546eb161555f8c6c8986f2162f85b5
Ajay Prajapati <ajay4android@gmail.com>
2019-06-22 22:13:45 +0530

    added customixed bottomnavigation item

---
+++ app/src/main/java/india/ajay/snackbarandbottomnavigationapp/BottomNavigationActivity.kt
@@ -0,0 +1,12 @@
+package india.ajay.snackbarandbottomnavigationapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomNavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+    }
+}
